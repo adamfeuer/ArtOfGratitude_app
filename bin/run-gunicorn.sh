6ffa93c13036999cc26cf36db521387eb0fb7b99
Adam Feuer <adamf@web0.artofgratitude.org>
2012-12-21 04:09:28 +0000

    updated run-gunicorn.sh script to load correct prod config

--- bin/run-gunicorn.sh
+++ bin/run-gunicorn.sh
@@ -4,5 +4,5 @@ GRATITUDE_ENV=/opt/gratitude
 GRATITUDE_HOME=$GRATITUDE_ENV/gratitude
 
 source $GRATITUDE_ENV/bin/activate
-python $GRATITUDE_HOME/bin/manage.py run_gunicorn --settings=gratitude.conf.prod.settings --bind 127.0.0.1:8080
+python $GRATITUDE_HOME/bin/manage.py run_gunicorn --settings=conf.prod.settings --bind 127.0.0.1:8080
 
