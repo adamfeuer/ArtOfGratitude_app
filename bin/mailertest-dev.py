88ea3e34b95bd495028836e9de6be68aa35d640e
Adam Feuer <adamf@pobox.com>
2013-01-02 12:34:25 -0800

    update mailertest-dev.py to use mailertest.py's code

--- bin/mailertest-dev.py
+++ bin/mailertest-dev.py
@@ -3,15 +3,22 @@
 from mailer import Mailer
 from mailer import Message
 
-message = Message(From="adam@adamfeuer.com",
-                  To="adamfeuer@gmail.com")
-message.Subject = "HTML email test - dev"
-message.Html = """<form method="post" action="http://localhost:8080/profile-landing/adamf">
+toAddress='adamfeuer@gmail.com'
+
+smtpServer='mailout.easydns.com'
+user='adamfeuer.com'
+password='b8PnU7HXD1f25D0m9V7478029n4hL6'
+fromAddress='adam@adamfeuer.com'
+
+message = Message(From=fromAddress,
+                  To=toAddress)
+message.Subject = "HTML email test - prod"
+message.Html = """<form method="post" action="http://artofgratitude.com/app/profile-landing/adamf">
 <p>
 <label for="id_text">Type in your gratitude for today (start with "I am grateful for..."):</label>
 <input id="id_text" type="text" maxlength="5000" name="text">
 </p>
 </form>"""
 
-sender = Mailer('mailout.easydns.com', usr='adamfeuer.com', pwd='b8PnU7HXD1f25D0m9V7478029n4hL6')
+sender = Mailer(smtpServer, usr=user, pwd=password, use_tls=True)
 sender.send(message)
