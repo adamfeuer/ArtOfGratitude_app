9afa0f6c5e5db8f860f02b57f3aa83cdc50c63cd
Adam Feuer <adamf@pobox.com>
2013-02-06 12:02:26 -0800

    updating runserver-dev so it will run on robert's machine

--- bin/runserver-dev.sh
+++ bin/runserver-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 # for development
-GRATITUDE_HOME=$WORKON_HOME/gratitude/gratitude
+GRATITUDE_HOME=$HOME/personal/proj/gratitude/gratitude
 python $GRATITUDE_HOME/bin/manage.py runserver --settings=gratitude.conf.dev.settings 127.0.0.1:8080
 
