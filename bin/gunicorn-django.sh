1e3812bb0b41ad3c1076cf71d84b723a9e30d8c3
Adam Feuer <adamf@pobox.com>
2013-03-14 13:49:28 -0700

    changed gunicorn umask to 0660 to ensure writability by gunicorn user

--- bin/gunicorn-django.sh
+++ bin/gunicorn-django.sh
@@ -2,7 +2,7 @@
 APP=gratitude
 VIRTUALENV=/opt/$APP
 PORT=8080
-LOGDIR=/var/log/gunicorn/
+LOGDIR=/var/log/gunicorn
 SETTINGS=conf.prod.settings
 PATH=/bin:/usr/bin
 USER=www-data
@@ -11,9 +11,10 @@ IP=127.0.0.1
 WORKERS=5
 NAME=django-gunicorn
 DESC=$NAME
-LOGFILE="$LOGDIR$NAME.log"
+LOGFILE="$LOGDIR/$NAME.log"
 PIDFILE="$VIRTUALENV/run/$NAME.pid"
-COMMAND="$VIRTUALENV/$APP/bin/manage.py run_gunicorn --user=$USER --group=$GROUP --workers=$WORKERS --bind=$IP:$PORT --pid=$PIDFILE --name=$NAME --log-file=$LOGFILE --log-level=info --settings=$SETTINGS"
+UMASK="0660"
+COMMAND="$VIRTUALENV/$APP/bin/manage.py run_gunicorn --user=$USER --group=$GROUP --workers=$WORKERS --bind=$IP:$PORT --pid=$PIDFILE --name=$NAME --log-file=$LOGFILE --umask $UMASK --log-level=info --settings=$SETTINGS"
 #COMMAND="$VIRTUALENV/$APP/bin/manage.py run_gunicorn --bind=$IP:$PORT --pid=$PIDFILE --name=$NAME --log-file=$LOGFILE --log-level=info --settings=$SETTINGS"
 
 source $VIRTUALENV/bin/activate
